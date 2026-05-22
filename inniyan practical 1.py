c9b298663e5554e656895975df9897657d266f86
inniyan <saravananinniyan@gmail.com>
2026-05-22 15:28:29 +0530

    Add files via upload

---
+++ inniyan practical 1.py
@@ -0,0 +1,51 @@
+'''A=int(input("enter A:"))
+B=int(input("enter B:"))
+print("addition:",A+B)
+print("subraction:",A-B)
+print("multipilcation:",A*B)
+print("division:",A/B)
+print("moduls:",A%B)
+print("floor division:",A//B)
+print("exponentiation:",A**B)
+
+A=int(input("enter A:"))
+B=int(input("enterB:"))
+print("area:",A*B)
+print("perimeter:",2*(A+B))
+
+A=int(input("enter A:"))
+B=int(input("enter B:"))
+C=int(input("enter C:"))
+print("avarage:",(A+B+C)/3)
+
+A=int(input("enter A:"))
+print("square root:",A**0.5)
+
+P=int(input("enter P:"))
+R=int(input("enter R:"))
+T=int(input("enter T:"))
+print("simple intrest:",(P*T*R)/100)
+
+x=10
+x+=5
+x-=3
+x*=2
+x/=4
+x%=2
+x**=3
+print(x)
+
+A=int(input("enter A:"))
+print("cuberoot:",A**(1/3))
+
+A=int(input("enter A:"))
+B=int(input("enter B:"))
+A,B=B,A
+print("A:",A,"B:",B)
+
+U=int(input("enter U:"))
+P=int(input("enter P:"))'''
+
+
+
+
