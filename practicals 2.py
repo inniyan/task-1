451cd2677b324f3f8d4a5cd2a7f7fba71a485568
inniyan <saravananinniyan@gmail.com>
2026-05-22 16:16:20 +0530

    Add files via upload

---
+++ practicals 2.py
@@ -0,0 +1,49 @@
+#Even or Odd
+A=int(input("enter A:"))
+if A%2==0:
+    print("even")
+else:
+    print("odd")
+
+#positive negative or zero
+A=int(input("enter A:"))
+if A>0:
+    print("positive")
+elif A<0:
+    print("negative")
+else:
+    print("zero")
+    
+#largest number
+A=int(input("enter A:"))
+B=int(input("enter B:"))
+if A>B:
+    print("A is largest")
+else:
+    print("B is largest")
+
+#largest of 3 numbers
+A=int(input("enter A:"))
+B=int(input("enter B:"))
+C=int(input("enter C:"))
+if A>B and A>C:
+    print("A is lARGEST")
+elif B>A and B>C:
+    print("B is largest")
+else:
+    print("c is largest")
+#voting eligibility
+A=int(input("enter A:"))
+if A>=18:
+    print("eligibile")
+else:
+    print ("not eligibile")
+#leap year
+A=int(input("enter A:"))
+if A%4==0:
+    print("leap year")
+else:
+    print("not a leap year")
+    
+    
+    
