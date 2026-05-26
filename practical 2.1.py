9263f8cc6394d848cac0920bd4c83449c1117298
inniyan <saravananinniyan@gmail.com>
2026-05-26 15:18:18 +0530

    Add files via upload

---
+++ practical 2.1.py
@@ -0,0 +1,24 @@
+#grade calculator
+'''M=int(input("enter marks M:"))
+if M>=90:
+    print("grade A")
+elif M>=70:
+    print("grade B")
+elif M>=50:
+    print("grade C")
+else:
+    print("fail")
+#divisible by 7
+N=int(input("enter number:"))
+if N %7==0:
+    print("divisible by 7")
+else:
+    print("not divisible by 7")'''
+#password check
+P=int(input("eneter password P:"))
+if P>=8:
+    print("strong password")
+else:
+    print("weak password")
+    
+    
